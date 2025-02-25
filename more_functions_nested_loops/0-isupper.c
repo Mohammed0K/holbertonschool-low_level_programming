b4d0ab11e0e1f1ddd2c982c67ad7c61d3a386b9f
Mohammed0K <M.H.D-6@hotmail.com>
2025-02-25 17:00:15 +0000

    update

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
-/*
- * _isupper  - return if C upper
- * Return c
- * @x data add
- *
+/**
+ * _isupper - return if c is upper
+ * Return: c
+ * @c: Data from user
  */
-
-int _isupper(void)
+int _isupper(int c)
 {
-	char x = 'C';
-
-	if (isupper(x))
-	{
-	return (1);
-	}
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 	return (0);
 }
