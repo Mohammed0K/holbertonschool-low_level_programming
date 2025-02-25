e324d9c2782053f79f04ed1929ee693a65a7f79a
Mohammed0K <M.H.D-6@hotmail.com>
2025-02-25 13:16:25 +0000

    add file

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/*
+ * _isupper  - return if C upper
+ * Return c
+ * @x data add
+ *
+ */
+
+int _isupper(void)
+{
+	char x = 'C';
+
+	if (isupper(x))
+	{
+	return (1);
+	}
+	return (0);
+}
