c61697e1e65c4725112522ee15426e4e5ade1538
Mohammed0K <M.H.D-6@hotmail.com>
2025-02-26 11:15:06 +0000

    update the file

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /*
- * maint void to rest number to 98
+ * int rest to number to 98
  * @n the value to rest the number
  */
 
-int main (void)
+int rest_to_number_98(int *c)
 {
-	int n;
 	*n = 98;
-	return (0);
 }
