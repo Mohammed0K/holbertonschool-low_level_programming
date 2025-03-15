f76415562c50e67f89dba96f046059da8cf8d470
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-15 01:30:43 +0000

    add and update file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,80 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * create_array - Creates an array of chars
- * @size: The number of elements in the array.
- * @c: The character used to initialize each element of the array.
- *
- * Return: A pointer to the newly created array, or NULL
+ *create_array - array for prints a string
+ *@size: number elements array
+ *@c: char
+ *Return: pointer
  */
-
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
-	char *array = malloc(size * sizeof(char));
+char *buffer;
+unsigned int position;
 
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-	for (unsigned int i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-	return (array);
+if (size == 0)
+{
+return (NULL);
 }
 
+/*Define values with malloc*/
+buffer = (char *) malloc(size * sizeof(c));
 
-/**
- * simple_print_buffer - Prints the content of a buffer in hexadecimal format.
- * @buffer: The array of characters to print.
- * @size: The number of elements in the buffer.
- */
-
-void simple_print_buffer(char *buffer, unsigned int size)
+if (buffer == 0)
 {
-	unsigned int i;
-
-	i = 1;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf("  ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
+return (NULL);
 }
 
-/**
- * main - Entry point of the program.
- *
- * Return: 0 on success, or 1 if memory allocation fails.
- */
-
-int main(void)
+else
 {
-	char *buffer;
+position = 0;
+while (position < size) /*While for array*/
+{
+*(buffer + position) = c;
+position++;
+}
 
-	buffer = create_array(98, 'H');
-	if (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	simple_print_buffer(buffer, 98);
-	free(buffer);
-	return (0);
+return (buffer);
+}
+
 }
