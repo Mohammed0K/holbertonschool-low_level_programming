ae66634bf7ee1b317f509883b7fd3b692859ca47
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-22 03:11:16 +0000

    update

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_poriners.h"
+#include <stddef.h>
+/**
+ * array_iterator - Execute a function>
+ * @array: Pointer to the array of integers.
+ * @size: The number of elements.
+ * @action: pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		atcion(array[i]);
+}
