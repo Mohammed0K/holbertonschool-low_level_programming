72a0e9e239eb3f4961eeb482810ec6c39e639bbc
Mohammed0K <M.H.D-6@hotmail.com>
2025-03-22 02:50:23 +0000

    update

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#icloude "function_pointers.h"
+
+/*
+ * print_name - Print a name using a given function.
+ * @name: The name to be printed.
+ * @f: A pointer to the function that print the name.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
